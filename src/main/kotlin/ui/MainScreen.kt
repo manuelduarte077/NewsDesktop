fd2b8b03331463885d10c68949b2f08809048f10
Manuel Duarte  <carlosd99m@gmail.com>
2024-01-04 11:18:53 -0600

    feat: show list article news

---
+++ src/main/kotlin/ui/MainScreen.kt
@@ -0,0 +1,52 @@
+package ui
+
+import MainContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.*
+import data.NewsApiClient
+import data.model.Article
+import io.ktor.client.plugins.*
+import kotlinx.coroutines.launch
+@Composable
+fun MainScreen() {
+
+    var articles by remember { mutableStateOf(emptyList<Article>()) }
+    var headerTitle by remember { mutableStateOf("Headlines") }
+    var searchedText by remember { mutableStateOf("") }
+
+    val scope = rememberCoroutineScope()
+
+    LaunchedEffect(searchedText) {
+        scope.launch {
+            try {
+                val newsData = if (searchedText.isNotEmpty()) {
+                    NewsApiClient.getSearchedNews(searchedText)
+                } else {
+                    NewsApiClient.getTopHeadlines()
+                }
+
+                articles = newsData.articles
+
+            } catch (e: ClientRequestException) {
+                println("Error fetching news: ${e.message}")
+            }
+        }
+    }
+
+    Row {
+        //SidePanel
+        SidePanel(onMenuSelected = {
+            headerTitle = it
+            searchedText = ""
+            articles = emptyList()
+        }, onNewsSearched = { _searchedText, _headerTitle ->
+            searchedText = _searchedText
+            headerTitle = _headerTitle
+            articles = emptyList()
+        })
+        //MainContent
+        MainContent(headerTitle, articles)
+    }
+
+}
+
